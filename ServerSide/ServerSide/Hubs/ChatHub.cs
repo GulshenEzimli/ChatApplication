57cbc019bfb063301b7ff8ac702c38ec56b423de
GulshenEzimli <gulshn.azimli@gmail.com>
2026-02-04 20:31:15 +0400

    Support send message from one client to any group clients

--- ServerSide/ServerSide/Hubs/ChatHub.cs
+++ ServerSide/ServerSide/Hubs/ChatHub.cs
@@ -48,23 +48,71 @@ namespace ServerSide.Hubs
 
         public override async  Task OnConnectedAsync()
         {
-            await Clients.Caller.SendAsync("listclients", ClientStore.Clients); 
+            await Clients.Caller.SendAsync("listclients", ClientStore.Clients);
+            await Clients.All.SendAsync("listgroups", GroupStore.Groups);
         }
 
         public async Task AddGroupAsync(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-            Group group = new Group(){ GroupName = groupName }; 
+            Group group = new Group(){ GroupName = groupName };
+            var client = ClientStore.Clients.FirstOrDefault(c => c.ConnectionId == Context.ConnectionId);
+            group.Clients.Add(client);
+
             GroupStore.Groups.Add(group);
             await Clients.All.SendAsync("listgroups", GroupStore.Groups);
         }
 
         public async Task JoinToGroupAsync(IEnumerable<string> groupNames)
         {
+            var client = ClientStore.Clients.FirstOrDefault(c => c.ConnectionId == Context.ConnectionId);
             foreach (var groupName in groupNames)
             {
-                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                var group = GroupStore.Groups.FirstOrDefault(g => g.GroupName == groupName);
+                bool result = group.Clients.Any(c => c.ConnectionId == client.ConnectionId);
+                
+                if(!result)
+                { 
+                    group.Clients.Add(client);
+                    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                }
+            }
+        }
+
+        public async Task GetGroupClients(List<string> groupNames)
+        {
+            if (groupNames.Count == 1 && groupNames[0] =="-1")
+            {
+                await Clients.Caller.SendAsync("listclients", ClientStore.Clients);
+            }
+
+            var clients = new List<Client>();
+            foreach (var groupName in groupNames)
+            {
+                var group = GroupStore.Groups.FirstOrDefault(g => g.GroupName == groupName);
+                if (group is not null && group?.Clients.Count > 0)
+                    clients.AddRange(group.Clients);
+            }
+
+            clients = clients.DistinctBy(c => c.NickName).ToList();
+            if (clients.Count > 0)
+            {
+                await Clients.Caller.SendAsync("listclients", clients);
+            }
+        }
+
+        public async Task SendMessageToGroupAsync(string message, List<string> groupNames)
+        {
+            string senderNickName = ClientStore.Clients.FirstOrDefault(c => c.ConnectionId == Context.ConnectionId)?.NickName ?? "";
+            if(senderNickName is "")
+            {
+                await Clients.Caller.SendAsync("getErrorMessage", "Kullanıcı bulunamadı. Lütfen tekrar giriş yapın.");
+                return;
+            }
+            foreach (var groupName in groupNames)
+            {
+                await Clients.Group(groupName).SendAsync("receiveMessage", new { Message = message, Client = $"{groupName}:{senderNickName}" });
             }
         }
     }
